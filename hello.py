98d887da3a7bce60ae180dc6f900369cda9fa6d8
Lucy Wyman <wyman.lucy@gmail.com>
2015-02-21 10:53:05 -0800

    Adding templates, css, index page, and gitignore

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/update')
+def update():
+    update_page()
+    return render_template('chart.html') 
 
 if __name__ == '__main__':
     app.run()
